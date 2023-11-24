b8c49550b1057e2f81a0dd66e58272d91198f318
mutonijacky <mutonijackygi@gmail.com>
2023-11-24 11:21:45 -0800

    presentation

---
+++ pre.js
@@ -0,0 +1,7 @@
+// Write a function that converts a temperature from Celsius to Fahrenheit
+function convert(celsius,tFahrenheit)
+{
+     tFahrenheit = (celsius * 9/5) + 32
+    console.log(tFahrenheit);
+}
+convert(10);
